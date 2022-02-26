f098b81d73c451ed8f70434fb7f1ae094c23353c
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-02-26 12:52:38 +0200

    Add global styles

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
