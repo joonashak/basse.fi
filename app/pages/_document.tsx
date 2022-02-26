e90ca767cf2f0baf166b1305e37bdd48bfb7a049
Joonas Häkkinen <joonas@joonashakkinen.fi>
2022-02-26 22:42:17 +0200

    Add SSR stylesheet

---
+++ app/pages/_document.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/jsx-key */
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+/**
+ * Taken from https://github.com/vercel/next.js/blob/canary/examples/with-styled-components/pages/_document.js
+ * Apparently have to use `getInitialProps`, see https://github.com/vercel/next.js/discussions/22065
+ */
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
