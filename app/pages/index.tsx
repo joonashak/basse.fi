a4def6f4379c213b6aff6f9a929b106390d1a7bc
joonashak <hakkijoo@gmail.com>
2023-03-24 23:17:58 +0200

    Bootstrap next v13

--- app/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.primary};
-`;
-
-const Home = () => <Title>Hello, world!</Title>;
-
-export default Home;
--- app/styles/theme.ts
+++
@@ -1,5 +0,0 @@
-const theme = {
-  colors: { primary: "red" },
-};
-
-export default theme;
